87749f3a4d71090c650404759c4e44d43dae32aa
unknown <ingdavidavila@hotmail.com>
2023-04-09 14:31:23 -0500

    second commit updated node

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import NavScrollExample from '../NavBar/NavBar';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <NavScrollExample />
       </header>
     </div>
     
